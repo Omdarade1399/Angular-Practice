9d3ded55512bf1bdca22f38fb4e342c3dbc1ab7c
Omdarade1399 <150209165+Omdarade1399@users.noreply.github.com>
2024-11-26 12:39:06 +0530

    learning form validation in angular

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss'
 })
 export class AdminComponent {
+  model: any = {};
+  cover!: string;
+  cover_file: any;
+  showError = false;
 
+  onFileSelected($event: any){
+    const file = $event.target.files[0];
+    if(file){
+      this.cover_file = file;
+      const reader = new FileReader();
+      reader.onload = () => {
+        const dataURL = reader.result!.toString();
+        this.cover = dataURL;
+      }
+      reader.readAsDataURL(file)
+      this.showError = false
+    }
+  }
+
+  onSubmit(form: NgForm) {
+    if(form.invalid || !this.cover){
+      form.control.markAllAsTouched();
+      if(!this.cover){
+        this.showError = true;
+      }
+      return;
+    }
+  }
 }
